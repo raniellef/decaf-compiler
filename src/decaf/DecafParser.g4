45555a7f849f05d032b6961bac975bbe99f62664
UniCeub <uniceub@localhost.localdomain>
2018-04-18 11:06:44 -0300

    Adicionando arquivos alterados

--- src/decaf/DecafParser.g4
+++ src/decaf/DecafParser.g4
@@ -10,4 +10,40 @@ options
   tokenVocab=DecafLexer;
 }
 
-program: TK_class ID LCURLY RCURLY EOF;
+program: 'class Program' '{' decl_campo* decl_metodo* '}'
+decl_campo: { tipo id | tipo id  '{' int_literal '{'}+, ';'
+decl_metodo: { tipo | void } id'{'|{ tipo id}+, | '}' block
+block: '{'decl_variavel*statment* '}'
+decl_variavel: tipo id +, ';'
+tipo: int | boolean
+statment: location asssign_op expr ';'
+| call_metodo ';'
+| if (expr) block | else block
+| for id = expr ',' expr block
+| return | expr | ';'
+| break
+| continue ';'
+| block
+assign_op: =
+| +=
+| -=
+call_metodo: nome_metodo '('| expr+ , ')'
+nome_metodo: id
+location: id
+| id '|' expr '|'
+expr: location
+|call_metodo
+|literal
+|expr bin_op expr
+|'-' expr
+| '!' expr
+|'('expr')'
+callout_arg:expr | string_literal
+bin_op: arith_op| rel_op| eq_op | cond_op
+arith_op: + | - | * | / | %
+rel_op: < | > | <= | >=
+eq_op: == | !=
+cond_op: && | ||
+litral: int_literal | char_literal | bool_literal
+
+
--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -2,9 +2,18 @@ package decaf;
 
 import java.io.*;
 //import antlr.Token;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 import java6035.tools.CLI.*;
 
 class Main {
@@ -31,7 +40,24 @@ class Main {
 
 		        			switch (token.getType())
 		        			{
-		        			case DecafLexer.ID:
+/*
+								case DecafLexer.IDENTIFIER:
+									type = " IDENTIFIER";
+									break;
+								case DecafLexer.CHAR_LITERAL:
+									type = " CHARLITERAL";
+									break;
+								case DecafLexer.STRING_LITERAL:
+									type = " STRINGLITERAL";
+									break;
+								case DecafLexer.INTEGER_LITERAL:
+									type = " INTLITERAL";
+									break;
+								case DecafLexer.BOOLEAN_LITERAL:
+									type = " BOOLEANLITERAL";
+									break;
+*/
+						case DecafLexer.ID:
 		        				type = " IDENTIFIER";
 		        				break;
 		        			
@@ -48,8 +74,9 @@ class Main {
 						case DecafLexer.BOOLEANLITERAL:
 							type = " BOOLEANLITERAL";
 							break;
-						}
-						System.out.println (token.getLine() + type + " " + text);
+
+		        			}
+		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
 		        		done = true;
         			} catch(Exception e) {
@@ -61,11 +88,37 @@ class Main {
         	}
         	else if (CLI.target == CLI.PARSE || CLI.target == CLI.DEFAULT)
         	{
-        		DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
-				CommonTokenStream tokens = new CommonTokenStream(lexer);
-        		DecafParser parser = new DecafParser(tokens);
-                parser.program();
-        	}
+        	    // Primeiro faz o parsing da cadeia
+                DecafLexer lexer = new DecafLexer(new ANTLRInputStream(inputStream));
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                DecafParser parser = new DecafParser(tokens);
+
+                // Adiciona as regras semÃ¢nticas
+                ParseTree tree = parser.program();
+
+                if (CLI.debug) {
+                    // Se estiver no modo debug imprime a Ã¡rvore de parsing
+                    // Create Tree View
+                    // Source: https://stackoverflow.com/questions/23809005/how-to-display-antlr-tree-gui
+
+
+                    //show AST in console
+                    System.out.println(tree.toStringTree(parser));
+
+                    //show AST in GUI
+                    JFrame frame = new JFrame("Antlr AST");
+                    JPanel panel = new JPanel();
+                    TreeViewer viewr = new TreeViewer(Arrays.asList(
+                            parser.getRuleNames()),tree);
+                    viewr.setScale(1.5);//scale a little
+                    panel.add(viewr);
+                    frame.add(panel);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setSize(200,200);
+                    frame.setVisible(true);
+                }
+
+            }
         	
         } catch(Exception e) {
         	// print the error:
